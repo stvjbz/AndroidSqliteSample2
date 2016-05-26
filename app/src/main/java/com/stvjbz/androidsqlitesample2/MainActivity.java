7399c37d3ebee59d6c6a19f435c47bd204d53cf7
stvjbz <stvjbz@outlook.com>
2016-05-27 00:50:11 +0900

    [add] OkHttp3.0.1 Androidの通信ライブラリ

--- app/src/main/java/com/stvjbz/androidsqlitesample2/MainActivity.java
+++ app/src/main/java/com/stvjbz/androidsqlitesample2/MainActivity.java
@@ -4,7 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,6 +46,50 @@ public class MainActivity extends AppCompatActivity {
             // Writing Contacts to log
             Log.d("Name: ", log);
         }
+
+        Request request = new Request.Builder()
+                //http://api.openweathermap.org/data/2.5/weather?APPID=<自身のAPPID>&q=Tokyo
+                .url("http://api.openweathermap.org/data/2.5/weather?APPID=6d8c3db06cd827052dee0e743082fdf1&q=Tokyo")
+                .get()
+                .build();
+
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .readTimeout(15 * 1000, TimeUnit.MILLISECONDS)
+                .writeTimeout(20 * 1000, TimeUnit.MILLISECONDS)
+                .connectTimeout(20 * 1000, TimeUnit.MILLISECONDS)
+                //.addNetworkInterceptor(new StethoInterceptor())
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                //Log.d("okhttp3",call.toString());
+                //Log.d("okhttp3",response.body().toString());
+                try {
+                    String result = response.body().string();
+                    JSONObject resJson = new JSONObject(result);
+                    JSONArray weathers = resJson.getJSONArray("weather");
+                    JSONObject weather = weathers.getJSONObject(0);
+                    String description = weather.getString("description");
+
+                    //res = description;
+//                    Log.d("okhttp3", "resJson: " + resJson.toString());
+//                    Log.d("okhttp3", "weathers: " + weathers.toString());
+//                    Log.d("okhttp3", "weather: " + weather.toString());
+                    Log.d("okhttp3", "description: " + description);
+
+                } catch(JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
     }
+
 }
 
